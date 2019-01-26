7e17ca5f8d5427de474e4fe8fef34c189efcb47e
Ronald Oussoren <ronaldoussoren@mac.com>
2019-01-26 22:22:09 +0100

    Use current version of napoleon as shipped with sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0,
 # -- General configuration -----------------------------------------------------
 
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'sphinxcontrib.napoleon', 'sphinx_autodoc_typehints']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
