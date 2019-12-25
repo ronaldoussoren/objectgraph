70000f18f9f5d9154e226f13aa4c28e1329b5b27
Ronald Oussoren <ronaldoussoren@mac.com>
2019-12-25 15:20:40 +0100

    Infra  updates
    - Tweak tox.ini for python 3.9
    - Tweak tox.ini for newer black versions
    - Add pre-commit hooks (should be in sync with tox configuration)

--- doc/conf.py
+++ doc/conf.py
@@ -1,42 +1,51 @@
 # -*- coding: utf-8 -*-
 #
 
-import sys, os
+import os
+import sys
+
 
 def get_version():
     fn = os.path.join(
         os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-        "objectgraph", "__init__.py")
+        "objectgraph",
+        "__init__.py",
+    )
     for ln in open(fn):
-        if ln.startswith('__version__'):
-            version = ln.split('=')[-1].strip().strip('"')
+        if ln.startswith("__version__"):
+            version = ln.split("=")[-1].strip().strip('"')
             return version
 
 
-sys.path.insert(0,
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # -- General configuration -----------------------------------------------------
 
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 'sphinx.ext.napoleon']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.napoleon",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The encoding of source files.
-source_encoding = 'utf-8'
+source_encoding = "utf-8"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'objectgraph'
-copyright = u'2019, Ronald Oussoren'
+project = u"objectgraph"
+copyright = u"2019, Ronald Oussoren"
 
 # The short X.Y version.
 version = get_version()
@@ -45,47 +54,48 @@ release = version
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
-exclude_trees = ['_build']
+exclude_trees = ["_build"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'nature'
+html_theme = "nature"
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
 html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {
-    '**': [ 'localtoc.html', 'links.html', 'donate.html', 'searchbox.html' ],
-}
+html_sidebars = {"**": ["localtoc.html", "links.html", "donate.html", "searchbox.html"]}
 
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'objectgraphdoc'
+htmlhelp_basename = "objectgraphdoc"
 
 
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-latex_paper_size = 'a4'
+latex_paper_size = "a4"
 
 # The font size ('10pt', '11pt' or '12pt').
-latex_font_size = '12pt'
+latex_font_size = "12pt"
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'objectgraph.tex', u'objectgraph Documentation',
-   u'Ronald Oussoren', 'manual'),
+    (
+        "index",
+        "objectgraph.tex",
+        u"objectgraph Documentation",
+        u"Ronald Oussoren",
+        "manual",
+    )
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {
-        'python': ('http://docs.python.org/', None),
-}
+intersphinx_mapping = {"python": ("http://docs.python.org/", None)}
 
 todo_include_todos = True
